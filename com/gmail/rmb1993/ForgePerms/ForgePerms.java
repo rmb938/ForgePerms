23affa13e8fec96a8fe5b6aacfbf8703a29d391d
Ryan <Ryan@129.21.117.108>
2013-01-12 20:19:10 -0500

    Fixed Loading SQL library

--- com/gmail/rmb1993/ForgePerms/ForgePerms.java
+++ com/gmail/rmb1993/ForgePerms/ForgePerms.java
@@ -20,8 +20,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
 @Mod(modid = "ForgePerms", name = "Forge Permissions", version = "0.1")
@@ -40,9 +38,10 @@ public class ForgePerms {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        File libFile = new File(libDir, "common-dbutils-1.5.jar");
-        
-        System.out.println("File: "+libFile.getPath());
+        File libFile = new File(libDir, "commons-dbutils-1.5.jar");
+        if (libFile.exists() == false) {
+            System.out.println("Need to download!");
+        }
         
         URLClassLoader ucl = (URLClassLoader) getClass().getClassLoader();
         RelaunchClassLoader actualClassLoader = new RelaunchClassLoader(ucl.getURLs());
@@ -52,82 +51,10 @@ public class ForgePerms {
             ex.printStackTrace();
         }
         try {
-            actualClassLoader.findClass("org.apache.commons.dbutils.QueryRunner");
+            Class qr = Class.forName("org.apache.commons.dbutils.QueryRunner");
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
-    }
-
-    private static void downloadFile(File libFile, String rootUrl, String realFilePath, String hash) {
-        try {
-            URL libDownload = new URL(String.format(rootUrl, realFilePath));
-            FMLRelaunchLog.info("Downloading file %s", libDownload.toString());
-            URLConnection connection = libDownload.openConnection();
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            connection.setRequestProperty("User-Agent", "FML Relaunch Downloader");
-            int sizeGuess = connection.getContentLength();
-            performDownload(connection.getInputStream(), sizeGuess, hash, libFile);
-            FMLRelaunchLog.info("Download complete");
-        } catch (Exception e) {
-            if (e instanceof RuntimeException) {
-                throw (RuntimeException) e;
-            }
-            FMLRelaunchLog.severe("There was a problem downloading the file %s automatically. Perhaps you "
-                    + "have an environment without internet access. You will need to download "
-                    + "the file manually or restart and let it try again\n", libFile.getName());
-            libFile.delete();
-            throw new RuntimeException("A download error occured", e);
-        }
-    }
-
-    private static ByteBuffer downloadBuffer = ByteBuffer.allocateDirect(1 << 22);
-    
-    private static void performDownload(InputStream is, int sizeGuess, String validationHash, File target) {
-        if (sizeGuess > downloadBuffer.capacity()) {
-            throw new RuntimeException(String.format("The file %s is too large to be downloaded by FML - the coremod is invalid", target.getName()));
-        }
-        downloadBuffer.clear();
-
-        int bytesRead, fullLength = 0;
-        try {
-            byte[] smallBuffer = new byte[1024];
-            while ((bytesRead = is.read(smallBuffer)) >= 0) {
-                downloadBuffer.put(smallBuffer, 0, bytesRead);
-                fullLength += bytesRead;
-            }
-            is.close();
-            downloadBuffer.limit(fullLength);
-            downloadBuffer.position(0);
-        } catch (InterruptedIOException e) {
-            // We were interrupted by the stop button. We're stopping now.. clear interruption flag.
-            Thread.interrupted();
-            return;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        try {
-            String cksum = generateChecksum(downloadBuffer);
-            if (cksum.equals(validationHash)) {
-                downloadBuffer.position(0);
-                FileOutputStream fos = new FileOutputStream(target);
-                fos.getChannel().write(downloadBuffer);
-                fos.close();
-            } else {
-                throw new RuntimeException(String.format("The downloaded file %s has an invalid checksum %s (expecting %s). The download did not succeed correctly and the file has been deleted. Please try launching again.", target.getName(), cksum, validationHash));
-            }
-        } catch (Exception e) {
-            if (e instanceof RuntimeException) {
-                throw (RuntimeException) e;
-            }
-            throw new RuntimeException(e);
-        }
-    }
-    
-    private static String generateChecksum(ByteBuffer buffer) {
-        return CertificateHelper.getFingerprint(buffer);
     }
 
     /**
@@ -159,16 +86,16 @@ public class ForgePerms {
      * @return
      */
     private static File setupLibDir(File mcDir) {
-        File libDir = new File(mcDir, "lib");
+        File libDir = new File(mcDir, "mods");
         try {
             libDir = libDir.getCanonicalFile();
         } catch (IOException e) {
-            throw new RuntimeException(String.format("Unable to canonicalize the lib dir at %s", mcDir.getName()), e);
+            throw new RuntimeException(String.format("Unable to canonicalize the mods dir at %s", mcDir.getName()), e);
         }
         if (!libDir.exists()) {
             libDir.mkdir();
         } else if (libDir.exists() && !libDir.isDirectory()) {
-            throw new RuntimeException(String.format("Found a lib file in %s that's not a directory", mcDir.getName()));
+            throw new RuntimeException(String.format("Found a mods file in %s that's not a directory", mcDir.getName()));
         }
         return libDir;
     }
