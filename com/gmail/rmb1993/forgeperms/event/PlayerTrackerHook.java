74d55f1a9d3ebc41ee76b838804eded58356dca1
Ryan <Ryan@129.21.117.108>
2013-01-12 23:12:29 -0500

    Started PlayerTrackerHook

---
+++ com/gmail/rmb1993/forgeperms/event/PlayerTrackerHook.java
@@ -0,0 +1,32 @@
+package com.gmail.rmb1993.forgeperms.event;
+
+import cpw.mods.fml.common.IPlayerTracker;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ *
+ * @author Ryan
+ */
+public class PlayerTrackerHook implements IPlayerTracker {
+
+    @Override
+    public void onPlayerLogin(EntityPlayer player) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onPlayerLogout(EntityPlayer player) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onPlayerChangedDimension(EntityPlayer player) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onPlayerRespawn(EntityPlayer player) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
