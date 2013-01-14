633c693d399cbd2da22e0bef82c48afed55aaa16
Favorlock <favorlock@gmail.com>
2013-01-13 22:01:16 -0800

    Add boolean variable for global config selection.

--- com/gmail/rmb1993/forgeperms/config/Configuration.java
+++ com/gmail/rmb1993/forgeperms/config/Configuration.java
@@ -1,5 +1,11 @@
 package com.gmail.rmb1993.forgeperms.config;
 
 public class Configuration {
+	
+	private boolean global;
+	
+	public void setGlobal(boolean global) {
+		this.global = global;
+	}
 
 }
