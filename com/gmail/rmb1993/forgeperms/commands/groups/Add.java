ac07a78d63f6fbcf3910b638915862514a6516f8
Favorlock <favorlock@gmail.com>
2013-01-23 14:18:12 -0800

    Set up /group add command.

--- com/gmail/rmb1993/forgeperms/commands/groups/Add.java
+++ com/gmail/rmb1993/forgeperms/commands/groups/Add.java
@@ -1,6 +1,10 @@
 package com.gmail.rmb1993.forgeperms.commands.groups;
 
 import com.gmail.rmb1993.forgeperms.ForgePermsContainer;
+import com.gmail.rmb1993.forgeperms.api.ForgePermsAPI;
+import com.gmail.rmb1993.forgeperms.permissions.user.User;
+import com.gmail.rmb1993.forgeperms.utils.StringColors;
+
 import net.minecraft.command.ICommandSender;
 
 /**
@@ -9,8 +13,23 @@ import net.minecraft.command.ICommandSender;
  */
 public class Add {
 
-    public Add(ForgePermsContainer fpc, ICommandSender var1, String[] var2) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Add(ForgePermsContainer fpc, ICommandSender sender, String[] args) {
+        if (args.length == 2) {
+        	User u = fpc.config.getDb().getUser(sender.getCommandSenderName());
+        	if (ForgePermsAPI.playerHasPermission(u.getUserName(), "permissions.addGroup")) {
+        		if (fpc.config.getDb().getGroup(args[2]) != null) {
+        			sender.sendChatToPlayer(StringColors.EnumTextColor.RED.colorString("Sorry the group " + args[2] + " already exist!"));
+        			return;
+        		}
+            	fpc.config.getDb().createGroup(args[2]);
+            	sender.sendChatToPlayer(StringColors.EnumTextColor.DARK_GREEN.colorString("You added the group " + args[2]));
+            	return;
+        	} else {
+        		sender.sendChatToPlayer(StringColors.EnumTextColor.RED.colorString("You do not have permission to use this command."));
+        	}
+        } else {
+        	sender.sendChatToPlayer(StringColors.EnumTextColor.RED.colorString("Usage: /group add [groupName]"));
+        }
     }
 
 }
